a763e690a91fbf5231f2d02996efbdbed146ea04
Abhishek-9653 <162132849+Abhishek-9653@users.noreply.github.com>
2024-03-04 15:14:18 +0000

    config done

---
+++ .devcontainer/Code_01/variable.js
@@ -0,0 +1,22 @@
+const Rollno = 1234
+//const can never get change after once done
+
+var Name = "James" 
+/*var is not used officially bcz it's scope issue +
+    and it also allow same name of two or more different vaiable 
+*/
+
+let email = "james@gmail.com"
+// it's best & popular
+
+City = "Chennai"
+// we can also create varibale like above but in informal ways only
+
+
+email = "james123@gmail.com"
+// we can cahnge var value 
+
+console.log(Rollno)
+console.log(Name)
+console.log(email)
+console.log(City)
